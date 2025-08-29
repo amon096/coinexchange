f1d35d1beac6c3f5833f4b49cb994d4452e8493f
amon <amon0967951290@gmail.com>
2025-08-29 16:04:16 +0700

    第一次提交

---
+++ coin-iaas/authorization-server/src/main/java/com/bjsxt/AuthorizationApplication.java
@@ -0,0 +1,11 @@
+package com.bjsxt;
+
+
+
+@SpringBootApplication
+public class AuthorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationApplication.class,args) ;
+    }
+}
